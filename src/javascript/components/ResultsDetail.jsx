99f9755bc59b829a030d8e64c291a7c6740c8820
Claus Wahlers <claus@codeazur.com.br>
2015-06-19 18:24:34 -0300

    display raw results in table

--- src/javascript/components/ResultsDetail.jsx
+++ src/javascript/components/ResultsDetail.jsx
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var React = require('react');
 var FluxibleMixin = require('fluxible/addons/FluxibleMixin');
 var EventsStore = require('../stores/EventsStore');
@@ -17,6 +18,45 @@ var ResultsDetail = React.createClass({
     onChange: function () {
     },
 
+    renderTable: function () {
+        var doc = this.getStore(EventsStore).getById(this.props.params.id);
+        var firstItem = doc.events[0];
+        var yTotal = 0;
+        var rows = _.map(doc.events, function(item, i) {
+            var time = (Math.round((item.time - firstItem.time) * 1000) / 1000).toFixed(3);
+            var timeDelta = ((i > 0) ? Math.round((item.time - doc.events[i - 1].time) * 1000) / 1000 : 0).toFixed(3);
+            var yDelta = (i > 0) ? item.y - doc.events[i - 1].y : 0;
+            yTotal += item.y;
+            return (
+                <tr key={Math.round(item.time * 1000)}>
+                    <td>{time}</td>
+                    <td>{timeDelta}</td>
+                    <td>{yTotal}</td>
+                    <td>{item.y}</td>
+                    <td>{yDelta}</td>
+                    <td>{item.mode}</td>
+                </tr>
+            );
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>ms</th>
+                        <th>∆ms</th>
+                        <th>y</th>
+                        <th>∆y</th>
+                        <th>∆∆y</th>
+                        <th>mode</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    },
+
     render: function() {
         var doc = this.getStore(EventsStore).getById(this.props.params.id);
         var date = new Date(doc.createdAtNum);
@@ -30,6 +70,7 @@ var ResultsDetail = React.createClass({
                 <div className="canvas-container">
                     <ChartDelta width={950} height={150} data={doc.events} />
                 </div>
+                {this.renderTable()}
             </div>
         );
     }
